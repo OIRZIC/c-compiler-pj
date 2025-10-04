57e56d8088871f57f95c2f175649168730fdb9dd
OIRZIC <maiphuocthinh301@gmail.com>
2025-10-04 11:16:14 +0700

    Next 2

---
+++ int.s
@@ -0,0 +1,15 @@
+	.file	"int.c"
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.section	.text.startup,"x"
+	.p2align 4,,15
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+	pushl	%ebp
+	movl	%esp, %ebp
+	andl	$-16, %esp
+	call	___main
+	movl	$2, %eax
+	leave
+	ret
+	.ident	"GCC: (MinGW.org GCC-6.3.0-1) 6.3.0"
